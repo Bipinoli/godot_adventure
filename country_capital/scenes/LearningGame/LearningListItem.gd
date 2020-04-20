a499355682e6965c0019491c95e6fe72fbbd97fd
bipinoli <bipinoli90@gmail.com>
2020-04-20 22:50:58 +0545

    label text auto scale down to avoid overflow in listitem

--- country_capital/scenes/LearningGame/LearningListItem.gd
+++ country_capital/scenes/LearningGame/LearningListItem.gd
@@ -5,9 +5,12 @@ signal listitem_clicked
 
 
 onready var flag = get_node("ColorRect/Flag")
-onready var country = get_node("ColorRect/VBoxContainer/Country")
-onready var capital = get_node("ColorRect/VBoxContainer/Capital")
+onready var country = get_node("ColorRect/Country")
+onready var capital = get_node("ColorRect/Capital")
 
+func _ready():
+#	_setCountry("united arab emirates and historical")
+	pass
 
 func _setup(country_val, capital_val, flag_name):
 	_setCountry(country_val)
@@ -21,14 +24,21 @@ func _set_flag(flag_texture):
 	flag.set_texture(flag_texture)
 
 func _setCountry(country_val):
-	print("country: ", country_val)
-	print("name: ", self.name)
+	# roughly 15 chars fit in one line so lets scale the text down 
+	# incase of overflow
 	country.set_text(country_val)
+	if len(country_val) > 15:
+		var scale: float = (15.0 / len(country_val))
+		print("--- country label overflow ----: ", country_val)
+		print("--- scaling by: ", scale)
+		country.set_scale(Vector2(scale, scale))
+		country.set_size(Vector2(country.get_size().x/scale, country.get_size().y/scale))
 
 func _setCapital(capital_val):
 	capital.set_text(capital_val)
+	
 
-
+ 
 func _getCountry():
 	return country.get_text()
 
